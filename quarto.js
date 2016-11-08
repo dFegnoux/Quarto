46146a7618f7fa8a99cd967ee34e499765bf5537
psa-dfegnoux <denis.fegnoux@ext.mpsa.com>
2016-11-08 14:02:57 +0100

    Quarto event logic + useless menu

--- quarto.js
+++ quarto.js
@@ -6,18 +6,22 @@ quarto = {
 	_init: function(options) {
 		this.options = this._extend(this._defaults, options);
 		this._createInterface();
-		this._setEvents();
+		this._dragEvents();
 	},
 	_createInterface: function() {
+		this.overlay = document.getElementById('overlay');
+		this.btnNewGame = document.getElementById('btn-new-game');
+
 		if(this.options.gameContainer) {
 			this.createGrid();
 			this.piecesContainer = document.createElement('div');
 			this.piecesContainer.className = 'pieces-container';
 			this.options.gameContainer.appendChild(this.piecesContainer);
 			this.createPieces();
+			this.interfaceEvents();
 		}
 	},
-	_setEvents: function() {
+	_dragEvents: function() {
 		var draggedElement;
 
 		document.addEventListener('dragstart', function(e) {
@@ -31,7 +35,7 @@ quarto = {
 
 		document.addEventListener("dragover", function(e) {
 			e.preventDefault();
-			if(e.target.tagName.toLowerCase() === 'td') {
+			if(draggedElement && e.target.tagName.toLowerCase() === 'td') {
 				e.dataTransfer.dropEffect = "move";
 				e.target.style.background = 'lightgrey';
 			}
@@ -39,7 +43,7 @@ quarto = {
 
 		document.addEventListener("dragleave", function(e) {
 			e.preventDefault();
-			if(e.target.tagName.toLowerCase() === 'td') {
+			if(draggedElement && e.target.tagName.toLowerCase() === 'td') {
 				e.target.style.background = 'white';
 			}
 		});
@@ -50,6 +54,7 @@ quarto = {
 			if(draggedElement && cell.tagName.toLowerCase() === 'td'){
 				cell.appendChild(draggedElement);
 				cell.style.background = 'white';
+				this.updateCell(cell, draggedElement);
 				this.winCheck(cell);
 			}
 			draggedElement.style.opacity = 1;
@@ -101,6 +106,11 @@ quarto = {
 			this.piecesContainer.appendChild(uiPiece);
 		}.bind(this));
 	},
+	updateCell: function(cell, piece) {
+		var particularities = piece.className.split(' ');
+		particularities.shift(); //Remove unwanted 'piece' first element to get only particularities
+		this.currentTable[cell.dataset.y][cell.dataset.x].dataset.piece = particularities;
+	},
 	setDragEvent: function() {
 		var cells = document.querySelectorAll('td');
 		for(var i = 0; i < cells.length; i++){
@@ -118,18 +128,52 @@ quarto = {
 		}
 	},
 	winCheck: function(playedCell) {
-		// var cellClasses = playedCell.className.split(' ');
-		// console.log(cellClasses);
-		// console.log(this.currentTable);
-		var relativesX = [],
-			relativesY = [];
+		var relatives = {
+			line: [],
+			column: [],
+			left2rightDiagonal: [],
+			right2leftDiagonal: []
+		};
+
+		// Fill relatives
 		for (i = 1; i <= 4; i++) {
-			console.log(playedCell);
-			relativesX.push(this.currentTable[playedCell.dataset.y][i]);
-			relativesY.push(this.currentTable[i][playedCell.dataset.x]);
+			relatives.line.push(this.currentTable[playedCell.dataset.y][i]);
+			relatives.column.push(this.currentTable[i][playedCell.dataset.x]);
+			relatives.left2rightDiagonal.push(this.currentTable[i][i]);
+			relatives.right2leftDiagonal.push(this.currentTable[5-i][i]);
 		}
 
-		console.log(relativesY, relativesX);
+		for (var type in relatives) {
+			var pieces = [];
+			relatives[type].forEach(function(cell){
+				if(cell.dataset.piece) {
+					pieces.push(cell.dataset.piece.split(','));
+				}
+			});	
+			if(pieces.length === 4) {
+				this.comparePieces(pieces);
+			}
+		}
+	},
+	comparePieces: function(pieces) {
+		
+		pieces[0].forEach(function(particularity) {
+			var commonCells = 1;
+			for(var i=1; i<=3; i++) {
+				if(pieces[i].indexOf(particularity) !== -1) {
+					commonCells++;
+				}
+			}
+			if(commonCells === 4) {
+				console.log('QUARTO');
+			}
+		});
+	},
+	interfaceEvents: function() {
+		this.btnNewGame.addEventListener('click', function(e) {
+			this.overlay.className += " hidden";
+			this.btnNewGame.parentElement.className += " hidden";
+		}.bind(this));
 	},
 	_extend : function(a, b) {
 		for(var key in b) {
